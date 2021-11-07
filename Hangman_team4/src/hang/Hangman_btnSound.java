b218f00afebc110564063b5bcc3cd933ab41da60
namGyunKim <skarbs01@naver.com>
2021-11-08 03:18:25 +0900

    연동성공!!

---
+++ Hangman_team4/src/hang/Hangman_btnSound.java
@@ -0,0 +1,47 @@
+package hang;
+
+import java.io.File;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+
+
+public class Hangman_btnSound {
+	public void abc() {
+	    File bgm;
+	    AudioInputStream stream;
+	    AudioFormat format;
+	    DataLine.Info info;
+	    
+	    bgm = new File("click.wav"); 
+	    
+	    Clip clip;
+	    
+	    try {
+	           stream = AudioSystem.getAudioInputStream(bgm);
+	           format = stream.getFormat();
+	           info = new DataLine.Info(Clip.class, format);
+	           clip = (Clip)AudioSystem.getLine(info);
+	           clip.open(stream);
+	           clip.start();
+	           
+	    } catch (Exception e) {
+	           System.out.println("err : " + e);
+	           }
+	    for(int i=0;i<1;i++) {
+	           try {
+	                 Thread.sleep(10);
+	                 break;
+	           } catch(Exception ee1) {
+	                 
+	           }
+	    }
+	    
+	}
+	public static void main(String[] args) {
+	
+	}
+}
